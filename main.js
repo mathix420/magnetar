802378f765aa0f041502f871e69f22712cf1b4c6
Arnaud Gissinger <agissing@student.42.fr>
2024-02-20 12:11:55 +0100

    [UPDATE] some stuff

--- main.js
+++ main.js
@@ -1,16 +1,24 @@
-const ws = new WebSocket('wss://magnetar.cloudno.de')
+const ws = new WebSocket('ws://localhost:8080')
 
 ws.onmessage = (message) => {
     console.info(message)
     const j = Object.entries(JSON.parse(message.data))
-    j.forEach(([ key, value ]) => {
+    j.forEach(([ key, { value, tokens } ]) => {
         if ( key.startsWith('v') ) {
             const elem = document.querySelector(`*[${key}]`)
             elem.removeAttribute('empty')
             elem[elem.attributes[key].value] = value
         } else {
             const elem = document.querySelector(`res[${key}]`)
-            elem.removeAttribute('empty')
+
+            if (tokens && tokens.length) {
+                window[tokens[0]] = () => {
+                    elem.removeAttribute('empty')
+                }
+            } else {
+                elem.removeAttribute('empty')
+            }
+
             elem.innerHTML = value
         }
     });
--- server.js
+++ server.js
@@ -6,22 +6,35 @@ const wss = new WebSocket.Server({ port: process.env.app_port || 8080 });
 wss.on('connection', function connection(ws) {
     ws.on('message', function incoming(message) {
         console.log('received: %s', message);
-        if ( message == 'getName' ) {
+        if (message == 'getName') {
             ws.send(JSON.stringify({
-                vres1: faker.name.findName(),
-                r2: `<h3>${new Date()}</h3>`
+                vres1: {
+                    value: faker.name.findName(),
+                },
+                r2: {
+                    value: `<h3>${new Date()}</h3>`
+                },
             }))
-        } else if ( message == 'getPic' ) {
+        } else if (message == 'getPic') {
             ws.send(JSON.stringify({
-                res3: `<img src="https://picsum.photos/640/480" />`,
-                r2: `<h3>${new Date()}</h3>`
+                res3: {
+                    value: `<img src="https://picsum.photos/640/480?t=${new Date().getTime()}" onload="onLoadCallback" />`,
+                    tokens: ['onLoadCallback'],
+                },
+                r2: {
+                    value: `<h3>${new Date()}</h3>`
+                },
             }))
         }
     });
 
     ws.send(JSON.stringify({
-        r1: '<h2 class="has-text-link">Secondary title</h2>',
-        r2: `<h3>${new Date()}</h3>`
+        r1: {
+            value: '<h2 class="has-text-link">Secondary title</h2>',
+        },
+        r2: {
+            value: `<h3>${new Date()}</h3>`
+        },
     }));
 
     setTimeout(() => sendUpdate(ws), 5000);
@@ -30,7 +43,11 @@ wss.on('connection', function connection(ws) {
 
 function sendUpdate(ws) {
     ws.send(JSON.stringify({
-        r1: '<h2 class="has-text-danger">This title has been updated dynamically from server</h2>',
-        r2: `<h3>${new Date()}</h3>`
+        r1: {
+            value: '<h2 class="has-text-danger">This title has been updated dynamically from server</h2>',
+        },
+        r2: {
+            value: `<h3>${new Date()}</h3>`
+        },
     }));
 }
